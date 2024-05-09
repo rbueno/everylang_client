244ee1d7b03c2e2a1fff6b9fb48bfd1a4e6c055a
rbueno <rafaelbuenolink@gmail.com>
2024-05-09 21:25:48 +0100

    general update

--- src/sections/@dashboard/student/StudentLessonInsight.js
+++ src/sections/@dashboard/student/StudentLessonInsight.js
@@ -24,6 +24,7 @@ import { AnalyticsConversionRates, HomeOptions, AnalyticsCurrentVisits } from '.
 import { BookingDetails } from '../general/booking'
 import { AppWidgetSummary } from '../general/app'
 import StudentPronunciationLessonExercises from './StudentPronunciationLessonExercises'
+import StudentGrammarLessonExercises from './StudentGrammarLessonExercises'
 import Label from '../../../components/label'
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { PATH_DASHBOARD } from '../../../routes/paths'
@@ -33,8 +34,154 @@ import { PATH_DASHBOARD } from '../../../routes/paths'
 import { _bookings, _bookingNew, _bookingsOverview, _bookingReview } from '../../../_mock/arrays';
 
 function StudentGrammarLessonInsight({ dataInsight }) {
-  return (<>Gramática</>)
+  const { push } = useRouter()
+
+  const handleBackToStudent = () => {
+  push(PATH_DASHBOARD.student.insight(dataInsight.student?._id));
 }
+  return (
+  <>
+  <Box marginBottom={4}>
+  {/* <Button variant='outlined' startIcon={<ArrowBackIcon />}>Voltar</Button> */}
+  <IconButton
+        size="small"
+        // color="inherit"
+        onClick={() => handleBackToStudent()}
+        // sx={{ color: 'text.secondary' }}
+      >
+        <ArrowBackIcon />
+      </IconButton>
+
+  </Box>
+  <Box marginBottom={4}>
+          <Typography variant='caption'>Aluno</Typography>
+          <Typography variant='h5'>{dataInsight.student?.fullName || '---'}</Typography>
+          
+        </Box>
+        
+         <Grid container spacing={3}>
+
+         
+
+
+          <Grid item xs={12} md={4} sm={4}>
+            <Card>
+              <CardHeader title='Lição de gramática' />
+              <CardContent>
+              <Box display='flex' flexDirection='row' alignItems='center' marginBottom={2}>
+                      {/* <Typography variant="subtitle2">Status</Typography> */}
+                      <Label sx={{ marginLeft: 1}} color={dataInsight.grammarExerciseSession[0].status === 'done' ? 'success' : 'warning'}>status: {dataInsight.grammarExerciseSession[0].status}</Label>
+                      <Label sx={{ marginLeft: 1}} color='success'>Pontuação: {dataInsight.exercisesPercent?.pronunciation?.averageScore}</Label>
+                    </Box>
+
+                    <Divider sx={{ margin: 2 }} />
+              <Box marginBottom={2}>
+                      <Typography variant="h6">Informação interna</Typography>
+
+          </Box>
+          <Box display='flex' flexDirection='column'>
+                    <Box display='flex' flexDirection='row' alignItems='center'>
+                      <Typography variant="subtitle2">- Título interno:</Typography>
+                      <Typography sx={{ marginLeft: 1}} variant="caption">{dataInsight.lesson?.internalTitle}</Typography>
+                    </Box>
+                    <Box display='flex' flexDirection='row' alignItems='center'>
+                      <Typography variant="subtitle2">- Idioma:</Typography>
+                      <Typography sx={{ marginLeft: 1}} variant="caption">{dataInsight.lesson?.learningLanguage}</Typography>
+                    </Box>
+                    <Box display='flex' flexDirection='row' alignItems='center'>
+                      <Typography variant="subtitle2">- Proficiência:</Typography>
+                      <Typography sx={{ marginLeft: 1}} variant="caption">{dataInsight.lesson?.languageLevel}</Typography>
+                    </Box>
+                   
+                    <Divider sx={{ margin: 2 }} />
+
+                    <Box marginBottom={2}>
+                      <Typography variant="h6">Informação pública</Typography>
+
+                   </Box>
+
+                   <Box display='flex' flexDirection='row' alignItems='center'>
+                      <Typography variant="subtitle2">- Criado por:</Typography>
+                      <Typography sx={{ marginLeft: 1}} variant="caption">{dataInsight.lesson?.creator}</Typography>
+                    </Box>
+
+                    
+                    <Box display='flex' flexDirection='row' alignItems='center'>
+                      <Typography variant="subtitle2">- Título público:</Typography>
+
+                      <Typography sx={{ marginLeft: 1}} variant="caption">{dataInsight.lesson?.internalTitle || '---'}</Typography>
+                    </Box>
+                    
+                    <Box display='flex' flexDirection='row' alignItems='center'>
+                      <Typography variant="subtitle2">- Descrição:</Typography>
+                      <Typography sx={{ marginLeft: 1}} variant="caption">{dataInsight.lesson?.description || '---'}</Typography>
+                    </Box>
+
+                   
+
+                   
+                  
+                    
+                   
+                 
+             
+                  </Box>
+              </CardContent>
+            </Card>
+          {/* <HomeOptions
+              title="Pontuação geral"
+              total={dataInsight.exercisesPercent?.pronunciation?.averageScore}
+              color="success"
+              py={3}
+              // icon={<VoiceChatIcon />}
+            />
+            <HomeOptions
+              title="Pontuação do speedy"
+              total={80}
+              color="warning"
+              py={3}
+              // icon={<VoiceChatIcon />}
+            /> */}
+          </Grid> 
+
+          <Grid item xs={12} md={8} sm={8}>
+          <AnalyticsConversionRates
+                title="Erros mais frequêntes de gramática"
+                subheader="Esses são os principais error de gramática que o aluno apresentou ao longo de todos os exercícios"
+                chart={{
+                  series: dataInsight.grammarToImprove,
+                }}
+              />
+          </Grid> 
+
+          {/* <Grid item xs={12} md={3} sm={6}>
+            <HomeOptions
+              title="Pontuação geral"
+              total={12}
+              color="warning"
+              py={3}
+              // icon={<VoiceChatIcon />}
+            />
+          </Grid>  */}
+
+          {/* <Grid item xs={12} md={3} sm={6}>
+          <HomeOptions
+              title="Pontuação geral"
+              total={12}
+              color="warning"
+              py={3}
+              // icon={<VoiceChatIcon />}
+            />
+          </Grid> */}
+
+            <Grid item xs={12}>
+            <StudentGrammarLessonExercises grammarExercises={dataInsight.grammarExercises} grammarAssessment={dataInsight.grammarAssessment}/>
+          </Grid>
+          </Grid>
+  </>
+)
+}
+
 function StudentPronunciationLessonInsight({ dataInsight }) {
 const { push } = useRouter()
 
@@ -72,7 +219,7 @@ const { push } = useRouter()
               <CardContent>
               <Box display='flex' flexDirection='row' alignItems='center' marginBottom={2}>
                       {/* <Typography variant="subtitle2">Status</Typography> */}
-                      <Label sx={{ marginLeft: 1}} color='success'>status: {dataInsight.pronunciationPracticeSession[0].status}</Label>
+                      <Label sx={{ marginLeft: 1}} color={dataInsight.pronunciationPracticeSession[0].status === 'done' ? 'success' : 'warning'}>status: {dataInsight.pronunciationPracticeSession[0].status}</Label>
                       <Label sx={{ marginLeft: 1}} color='success'>Pontuação: {dataInsight.exercisesPercent?.pronunciation?.averageScore}</Label>
                     </Box>
 
