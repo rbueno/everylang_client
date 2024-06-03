f7133fc89b9149be842c1ad9d49e28faa9a22dea
rbueno <rafaelbuenolink@gmail.com>
2024-06-03 18:18:34 +0100

    first commit

--- src/pages/dashboard/home.js
+++ src/pages/dashboard/home.js
@@ -106,11 +106,14 @@ const stepsAccessGuideHome = [
     content: (
       <div>
       <h3>Vamos criar a sua primeira lição</h3>
-      <p>Clique aqui para conferir um passo a passo</p>
-      {/* <Stack m={2}>
-      <Button variant='contained' onClick={() => handleNextCreatePronunciaionLesson()}>Criar lição de pronúncia</Button>
+      <p>Clique aqui para conferir um passo a passo de como criar uma lição. Para esse passo a passo, criaremos uma lição de pronúncia.</p>
+      <Stack m={2}>
+      {/* <Button variant='contained' onClick={() => handleNextCreatePronunciaionLesson()}>Criar lição de pronúncia</Button> */}
+      <Button fullWidth size='large' href={PATH_DASHBOARD.lessonPronunciation.new} rel="noopener" variant="contained">
+      Criar lição de pronúncia
+      </Button>
 
-      </Stack> */}
+      </Stack>
       </div>
     ),
     disableBeacon: true,
